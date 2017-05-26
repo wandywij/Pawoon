9677ce936bdf5c2dfdb4f0de4e299bba3dc62782
Wandy Wijayanto <wandy.wijayanto@bridestory.com>
2017-05-26 08:12:58 +0700

    add navigaton and butterknife

--- app/src/main/java/com/wnd/pawoon/MainActivity.java
+++ app/src/main/java/com/wnd/pawoon/MainActivity.java
@@ -1,8 +1,11 @@
 package com.wnd.pawoon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.wnd.pawoon.model.ToDoModel;
 import com.wnd.pawoon.presenter.MainActivityPresenter;
@@ -10,15 +13,21 @@ import com.wnd.pawoon.view.MainView;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity implements MainView{
 
     private MainActivityPresenter presenter;
+    @BindView(R.id.toDoListButton)
+    public Button toDoListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -31,5 +40,11 @@ public class MainActivity extends AppCompatActivity implements MainView{
 
     @Override
     public void onFetched(List<ToDoModel> toDoModels) {
+    }
+
+    @OnClick(R.id.toDoListButton)
+    public void navigate() {
+        Intent i = new Intent(this, ToDoListActivity.class);
+        startActivity(i);
     }
 }
